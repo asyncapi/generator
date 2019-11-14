38252b374c0c272bec64713fcda03ba3d1dbceb3
jonas lagoni <jonas-lt@live.dk>
2019-11-14 23:25:15 +0100

    Added partial channels for request/reply and pubsub.

--- templates/nats-ts/src/channels/$$channel$$.ts
+++ templates/nats-ts/src/channels/$$channel$$.ts
@@ -1,46 +1,11 @@
-import * as WebsocketConnections from '../Client'
-import {{ channel.publish().message(0).name() }} from '../structure/{{ channel.publish().message(0).name() | kebabCase}}'
-export { { { channel.publish().message(0).name() } } as structure };
 
 {% if channel.hasPublish() %}
-{% if channel.publish().tags() | length == 0 or channel.publish().tags() | containsTag("server") %}
-/**
- * {{ channel.publish().summary() }}
- * @param {% raw %}{{% endraw %}{{ channel.publish().message(0).name() }}{% raw %}}{% endraw %} {{ channel.publish().message(0).name() | firstLowerCase}}
-*/
-export function publish(
-  {{ channel.publish().message(0).name() | firstLowerCase }}: { { channel.publish().message(0).name() } }
-    ) {
-  WebsocketConnections.emitMessageToClients(
-    WebsocketConnections.Channel.{{ channelName | kebabCase}},
-{ { channel.publish().message(0).name() | firstLowerCase } }
-    );
-}
+import {{ channel.publish().message(0).name() | camelCase }} from '../message/{{ channel.publish().message(0).name() | kebabCase}}'
 {% - endif %}
-{% - endif %}
-
 {% if channel.hasSubscribe() %}
-{% if channel.publish().tags() | length == 0 or channel.publish().tags() | containsTag("client") %}
-export interface CallbackObject {
-  cb: ({{ channel.publish().message(0).name() | firstLowerCase }}: { { channel.publish().message(0).name() } }) => void
-}
+import {{ channel.subscribe().message(0).name() | camelCase }} from '../message/{{ channel.subscribe().message(0).name() | kebabCase}}'
+{% - endif %}
 
-/**
- * {{ channel.publish().summary() }}
- * @param {% raw %}{{% endraw %}{{ channel.publish().message(0).name() }}{% raw %}}{% endraw %} {{ channel.publish().message(0).name() | firstLowerCase}}
-*/
-export function subscribe(
-  cb: CallbackObject
-) {
-  cbList.push(cb);
-}
-const cbList: CallbackObject[] = []
-export function notify(
-  {{ channel.publish().message(0).name() | firstLowerCase }}: { { channel.publish().message(0).name() } }
-    ) {
-  cbList.forEach((cbo: CallbackObject) => {
-    cbo.cb({{ channel.publish().message(0).name() | firstLowerCase }});
-});
-}
-{% - endif %}
-{% - endif %}
+import { Client, NatsError, Subscription, SubscriptionOptions, Payload } from 'ts-nats';
+import * as utils from '../utils';
+

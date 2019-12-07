0577c0aae14de894ee0f768e6d54d40350d1731b
jonas lagoni <jonas-lt@live.dk>
2019-12-07 19:32:39 +0100

    Renamed test files to .spec.js

---
+++ test/integration/templates/java-spring.spec.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+const dircompare = require('dir-compare');
+const options = { compareSize: true };
+const pathToExpectedTemplates = './test/templates/java-spring';
+const pathToactualTemplates = './test/integration/templates/actual/java-spring';
+const { exec } = require('child_process');
+const fs = require('fs');
+const Path = require('path');
+const format = require('util').format;
+var expect = require('chai').expect;
+const deleteFolderRecursive = function(path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach((file, index) => {
+      const curPath = Path.join(path, file);
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+describe('java-spring', () => {
+  before(() => {
+    deleteFolderRecursive(pathToactualTemplates);
+  });
+  it('no parameters', () => {
+    return new Promise((resolve, reject) => {
+      const child = exec(`node ./cli.js --output ${pathToactualTemplates}/no-params ./test/docs/streetlights.yml java-spring`);
+      child.stderr.pipe(process.stderr);
+      child.on('error', (e) => {
+        reject(e);
+      });
+      child.on('exit', (code) => {
+        if (code === 0) {
+          const res = dircompare.compareSync(pathToactualTemplates+"/no-params", `${pathToExpectedTemplates}/no-params`, options);
+          try{
+            expect(res.same).to.be.true;
+            resolve();
+            deleteFolderRecursive(pathToactualTemplates);
+          }catch(e){
+            reject(e);
+          }
+        } else {
+          reject(new Error(`Error code ${code} was returned`));
+        }
+      });
+    });
+  });
+});

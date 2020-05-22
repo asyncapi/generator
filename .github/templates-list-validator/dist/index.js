535d745a9cd743ae1fb8f87f588644d38c506489
derberg <lpgornicki@gmail.com>
2020-05-22 09:54:25 +0200

    update bundle with new error message

--- .github/templates-list-validator/dist/index.js
+++ .github/templates-list-validator/dist/index.js
@@ -509,10 +509,11 @@ const { readFileSync } = __webpack_require__(747);
 async function run() {
   try {
     const token = process.env.GITHUB_TOKEN || core.getInput('token', { required: true });
+    //TODO we should not only check the content of the readme but also the content of the website listing from https://github.com/asyncapi/website/blob/master/content/docs/tooling.md
     const templatesListContent = getReadmeContent();
     const officialTemplates = await searchOfficialTemplates(token);
 
-    const missingTemplates = officialTemplates.filter(str => templatesListContent.indexOf(str) === -1);
+    const missingTemplates = officialTemplates.filter(str => !templatesListContent.includes(str));
 
     if (missingTemplates) core.setFailed(
         `The following templates are not in the README.md: ${missingTemplates}. Make sure missing templates are added to the list in the README of this repository and also to the website to the list of offecial tools: https://github.com/asyncapi/website/blob/master/content/docs/tooling.md`
@@ -539,7 +540,9 @@ function getReadmeContent() {
 
   if (endOfOpeningTagIndex === -1 || endOfClosingTagIndex === -1) {
     core.setFailed();
-    throw new Error('Templates list is missing or someone removed markers that indicate the list');
+    throw new Error(
+        `Templates list is missing or someone removed markers that indicate the list. Table with list of templates that is in the README should be wrapped in markers like <!-- TEMPLATES-LIST:START --> and <!-- TEMPLATES-LIST:END -->`
+    );
   }
 
   return readmeContent.slice(endOfOpeningTagIndex + closingTag.length, endOfClosingTagIndex);
@@ -572,6 +575,7 @@ async function searchOfficialTemplates(token) {
 }
 
 run();
+
 
 /***/ }),
 

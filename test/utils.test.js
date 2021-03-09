74aa4af464a046e2afaa31c8edbb0b29ec08bd00
derberg <lpgornicki@gmail.com>
2021-03-09 23:22:01 +0100

    remoe not needed const

--- test/utils.test.js
+++ test/utils.test.js
@@ -47,7 +47,7 @@ describe('Utils', () => {
       utils.isFileSystemPath = jest.fn(() => false);
       resolvePkg.__resolvePkgValue = undefined;
       resolveFrom.__resolveFromValue = path.join(Generator.DEFAULT_TEMPLATES_DIR, templateNpmName, 'package.json');
-      const result = utils.getTemplateDetails(templateNpmName, 'package.json');
+      utils.getTemplateDetails(templateNpmName, 'package.json');
       expect(log.debug).not.toHaveBeenCalledWith('Remember that your local template must have its own node_modules installed first, \"npm install\" is not triggered by the generator.');
       expect(log.debug).toHaveBeenCalledWith(`${templateNpmName} not found in local dependencies but found it installed as a global package.`);
     });

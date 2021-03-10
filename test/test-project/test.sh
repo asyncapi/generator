26168d945331d746fd4629fce037c73f4e0f0da4
derberg <lpgornicki@gmail.com>
2021-03-10 17:39:02 +0100

    fixing permissions issues

--- test/test-project/test.sh
+++ test/test-project/test.sh
@@ -10,6 +10,6 @@ npm run test:project
 #installing html template globally before global tests
 PUPPETEER_SKIP_DOWNLOAD=true npm install -g @asyncapi/html-template@0.16.0
 #remove previously installed html template to make sure it is not picked up in the test
-rm -rf /node_modules/@asyncapi/html-template
+rm -rf node_modules/@asyncapi/html-template
 rm -rf ../../node_modules/@asyncapi/html-template
 npm run test:global

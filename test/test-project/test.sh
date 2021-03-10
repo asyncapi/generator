cfd1cc71caf8472dc65ae6bf8c80276ceb349db9
derberg <lpgornicki@gmail.com>
2021-03-10 17:12:57 +0100

    better tests readme I think

--- test/test-project/test.sh
+++ test/test-project/test.sh
@@ -6,7 +6,7 @@ cd app
 npm install
 cd test/test-project
 npm install
-npm test
+npm test:project
 #installing html template globally before global tests
 npm install -g @asyncapi/html-template@0.16.0
 #remove previously installed html template to make sure it is not picked up in the test

907a30f2a67e56ee71507b58acf54b4b2b447923
aayushRedHat <aaayush@redhat.com>
2024-01-09 20:13:39 +0530

    Update the description

--- test/test-project/test-registry.test.js
+++ test/test-project/test-registry.test.js
@@ -11,7 +11,7 @@ const mainTestResultPath = 'test/temp/integrationTestResult';
 //we do not want to download chromium for html-template if it is not needed
 process.env['PUPPETEER_SKIP_CHROMIUM_DOWNLOAD'] = true;
 
-describe('Integration testing generateFromFile() to make sure the result of the generation is not changend comparing to snapshot', () => {
+describe('Integration testing generateFromFile() to make sure the template can be download from the private repository.', () => {
   const generateFolderName = () => {
     //you always want to generate to new directory to make sure test runs in clear environment
     return path.resolve(mainTestResultPath, crypto.randomBytes(4).toString('hex'));
@@ -21,8 +21,8 @@ describe('Integration testing generateFromFile() to make sure the result of the 
 
   it('generated using private registory', async () => {
     const outputDir = generateFolderName();
-    const generator = new Generator('@asyncapi/html-template', outputDir, 
-      { forceWrite: true, templateParams: { singleFile: true }, 
+    const generator = new Generator('@asyncapi/html-template', outputDir,
+      { forceWrite: true, templateParams: { singleFile: true },
         registry: {url: 'http://localhost:4873/', username: 'admin', password: 'nimbda'}});
     await generator.generateFromFile(dummySpecPath);
     const file = await readFile(path.join(outputDir, 'index.html'), 'utf8');

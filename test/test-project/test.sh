4a2146b941d02f9f4f5dac7bcb67a48ec0dfe4c5
aayushRedHat <aaayush@redhat.com>
2024-01-15 22:27:04 +0530

    Update testing

--- test/test-project/test.sh
+++ test/test-project/test.sh
@@ -1,40 +1,17 @@
 #!/bin/bash
 
-# Function to install and test a specific project
-function test_project {
-
-  npm run test:project
-  # Installing html template globally before global tests
-  PUPPETEER_SKIP_DOWNLOAD=true npm install -g @asyncapi/html-template@0.16.0
-  # Remove previously installed html template to make sure it is not picked up in the test
-  rm -rf node_modules/@asyncapi/html-template
-  rm -rf ../../node_modules/@asyncapi/html-template
-  npm run test:global
-}
-
-# Function to test the registry
-function test_registry {
-  npm run test:registry
-}
-
-# Required by GitHub Actions
+#required by GitHub Actions
 sudo chown -R 1001:121 "/root/.npm"
-
-# Always run these steps
 cd app
-PUPPETEER_SKIP_DOWNLOAD=true npm install
+npm install
 cd test/test-project
-PUPPETEER_SKIP_DOWNLOAD=true npm install
-
-
-# Run the functions based on the provided arguments
-case "$1" in
-  "test-project")
-    test_project
-    test_registry
-    ;;
-  *)
-    echo "Invalid argument. Supported arguments: test-project"
-    exit 1
-    ;;
-esac
+PUPPETEER_SKIP_DOWNLOAD=true npm install @asyncapi/html-template@0.16.0
+npm install
+npm run test:project
+#installing html template globally before global tests
+PUPPETEER_SKIP_DOWNLOAD=true npm install -g @asyncapi/html-template@0.16.0
+#remove previously installed html template to make sure it is not picked up in the test
+rm -rf node_modules/@asyncapi/html-template
+rm -rf ../../node_modules/@asyncapi/html-template
+npm run test:global
+npm run test:registry

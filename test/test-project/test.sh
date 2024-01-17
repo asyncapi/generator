0d2e51fc6c0db1eedac1382b0945c718ea69fcb6
aayushRedHat <aaayush@redhat.com>
2024-01-17 15:23:42 +0530

    changed testing

--- test/test-project/test.sh
+++ test/test-project/test.sh
@@ -7,7 +7,6 @@ npm install
 cd test/test-project
 PUPPETEER_SKIP_DOWNLOAD=true npm install @asyncapi/html-template@0.16.0
 npm install
-npm run test:project
 #installing html template globally before global tests
 PUPPETEER_SKIP_DOWNLOAD=true npm install -g @asyncapi/html-template@0.16.0
 #remove previously installed html template to make sure it is not picked up in the test

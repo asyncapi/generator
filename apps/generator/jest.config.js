d1d54f6c0a9a65aebe9a830cd3a94f057ef20c71
AyushNautiyalDeveloper <ayush@betalectic.com>
2024-04-11 22:19:50 +0530

    updated the correct jest config directory for the nimma package.

--- apps/generator/jest.config.js
+++ apps/generator/jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   clearMocks: true,
   moduleNameMapper: {
-    '^nimma/legacy$': '<rootDir>/node_modules/nimma/dist/legacy/cjs/index.js',
-    '^nimma/(.*)': '<rootDir>/node_modules/nimma/dist/cjs/$1',
+    "^nimma/legacy$":
+      "<rootDir>../../node_modules/nimma/dist/legacy/cjs/index.js",
+    "^nimma/(.*)": "<rootDir>../../node_modules/nimma/dist/cjs/$1",
   },
 };

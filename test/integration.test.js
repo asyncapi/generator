c0b9ec8d4f945bc44bced79930ed673e7bb3aa0a
lmgyuan <milleryuan0107@gmail.com>
2024-04-18 09:32:51 -0400

    edit generator parameter

--- test/integration.test.js
+++ test/integration.test.js
@@ -68,7 +68,6 @@ describe('Integration testing generateFromFile() to make sure the result of the 
     // based on the html-template documentation, the default output is index.html
     const generator = new Generator('@asyncapi/html-template@0.28.0', outputDir, {
       forceWrite: true,
-      outFilename: 'index.html',
       noOverwriteGlobs: ['**/index.html']
     });
 

833360035f83b615e09488e91bbf2502d2c7a667
lmgyuan <milleryuan0107@gmail.com>
2024-04-18 08:55:37 -0400

    Update integration.test.js

--- test/integration.test.js
+++ test/integration.test.js
@@ -9,6 +9,7 @@ const dummySpecPath = path.resolve(__dirname, './docs/dummy.yml');
 const refSpecPath = path.resolve(__dirname, './docs/apiwithref.json');
 const refSpecFolder = path.resolve(__dirname, './docs/');
 const crypto = require('crypto');
+const {writeFileSync, readFileSync} = require('node:fs');
 const mainTestResultPath = 'test/temp/integrationTestResult';
 //we do not want to download chromium for html-template if it is not needed
 process.env['PUPPETEER_SKIP_CHROMIUM_DOWNLOAD'] = true;
@@ -47,5 +48,26 @@ describe('Integration testing generateFromFile() to make sure the result of the 
     await generator.generateFromFile(refSpecPath);
     const file = await readFile(path.join(outputDir, 'index.html'), 'utf8');
     expect(file).toMatchSnapshot();
+  });
+
+  it('should ignore specified files with noOverwriteGlobs', async () => {
+    const outputDir = generateFolderName();
+    // Manually create a file to test if it's not overwritten
+    const testFilePath = path.join(outputDir, 'index.html');
+    // eslint-disable-next-line sonarjs/no-duplicate-string
+    writeFileSync(testFilePath, '<script>const initialContent = "This should not change";</script>');
+
+    // based on the html-template documentation, the default output is index.html
+    const generator = new Generator('@asyncapi/html-template@0.28.0', outputDir, {
+      forceWrite: true,
+      outFilename: 'index.html',
+      noOverwriteGlobs: ['**/index.html']
+    });
+
+    generator.generateFromFile(dummySpecPath);
+
+    // Read the file to confirm it was not overwritten
+    const fileContent = readFileSync(testFilePath, 'utf8');
+    expect(fileContent).toBe('<script>const initialContent = "This should not change";</script>');
   });
 });

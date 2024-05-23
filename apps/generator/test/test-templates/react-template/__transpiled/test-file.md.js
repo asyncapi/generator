16cfdb2abe4bfd565b93c687fe25f19c55e86d11
AyushNautiyalDeveloper <ayush@betalectic.com>
2024-05-23 16:51:04 +0530

    Added the required changes for the lint and command for the lint.

--- apps/generator/test/test-templates/react-template/__transpiled/test-file.md.js
+++ apps/generator/test/test-templates/react-template/__transpiled/test-file.md.js
@@ -1,21 +1,21 @@
 'use strict';
 
 require('source-map-support/register');
-var generatorReactSdk = require('@asyncapi/generator-react-sdk');
-var jsxRuntime = require('react/cjs/react-jsx-runtime.production.min');
+const generatorReactSdk = require('@asyncapi/generator-react-sdk');
+const jsxRuntime = require('react/cjs/react-jsx-runtime.production.min');
 
 function testFile_md ({
   asyncapi,
   params
 }) {
   return /*#__PURE__*/jsxRuntime.jsxs(generatorReactSdk.File, {
-    name: "test-file.md",
+    name: 'test-file.md',
     children: [/*#__PURE__*/jsxRuntime.jsx(generatorReactSdk.Text, {
-      children: "This is a markdown file for my application."
+      children: 'This is a markdown file for my application.'
     }), /*#__PURE__*/jsxRuntime.jsxs(generatorReactSdk.Text, {
-      children: ["App name is: **", asyncapi.info().title(), "**"]
+      children: ['App name is: **', asyncapi.info().title(), '**']
     }), /*#__PURE__*/jsxRuntime.jsxs(generatorReactSdk.Text, {
-      children: ["Version ", params.version, " running on ", params.mode, " mode "]
+      children: ['Version ', params.version, ' running on ', params.mode, ' mode ']
     })]
   });
 }

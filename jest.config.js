09e78823a00a214045809f940942a774076a353a
lmgyuan <milleryuan0107@gmail.com>
2024-05-24 17:35:44 -0400

    Revert "fixed logging issue"
    This reverts commit 3e7afe70ad13f4cdfacf5211e8d6c433f75f24b4.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   clearMocks: true,
-  modulePathIgnorePatterns: ["./__mocks__(?!/loglevel.js)"],
   moduleNameMapper: {
     '^nimma/legacy$': '<rootDir>/node_modules/nimma/dist/legacy/cjs/index.js',
     '^nimma/(.*)': '<rootDir>/node_modules/nimma/dist/cjs/$1',
--- test/integration.test.js
+++ test/integration.test.js
@@ -15,7 +15,6 @@ const {exists, writeFile} = require('../lib/utils');
 const mainTestResultPath = 'test/temp/integrationTestResult';
 const reactTemplate = 'test/test-templates/react-template';
 const nunjucksTemplate = 'test/test-templates/nunjucks-template';
-const logMessage = require('../lib/logMessages');
 const log = require('loglevel');
 
 describe('Integration testing generateFromFile() to make sure the result of the generation is not changend comparing to snapshot', () => {
@@ -68,12 +67,12 @@ describe('Integration testing generateFromFile() to make sure the result of the 
   });
 
   it('should ignore specified files with noOverwriteGlobs', async () => {
-    const logSpyDebug = jest.spyOn(log, 'debug').mockImplementation(() => {});
-    // log.debug = jest.fn(); 
+    // const logSpyDebug = jest.spyOn(log, 'debug').mockImplementation(() => {});
+    log.debug = jest.fn(); 
 
     const outputDir = generateFolderName();
     // Manually create a file to test if it's not overwritten
-    if (!existsSync(outputDir)) {
+    if (!await exists(outputDir)) {
       mkdirSync(outputDir, { recursive: true });
     }
     // Create a variable to store the file content
@@ -96,9 +95,15 @@ describe('Integration testing generateFromFile() to make sure the result of the 
     // Check if the files have been overwritten
     await expect(fileContent).toBe(testContent);
     // Check if the log debug message was printed
-    // await expect(logSpyDebug).toHaveBeenCalledWith(`noOverwriteGlobs matched`);
-    await expect(logSpyDebug).toHaveBeenCalledWith(logMessage.skipOverwriting(testFilePath));
+    await expect(log.debug).toHaveBeenCalledWith(`noOverwriteGlobs matched`);
+    await expect(log.debug).toHaveBeenCalledWith(`Skipping overwrite for: ${testFilePath}`);
+    // await console.log('All console.log calls:');
 
-    logSpyDebug.mockRestore();
+    // // Print all console.log calls
+    // await log.mock.calls.forEach((call, index) => {
+    //   console.log(`${index + 1}:`, call);
+    // });
+
+    log.mockRestore();
   });
 });

0385f26edba7e6665866c66fc3b5a5581215fc6f
lmgyuan <milleryuan0107@gmail.com>
2024-05-24 17:50:21 -0400

    eliminated unnecessary comments and codes;

--- test/integration.test.js
+++ test/integration.test.js
@@ -15,6 +15,7 @@ const {exists, writeFile} = require('../lib/utils');
 const mainTestResultPath = 'test/temp/integrationTestResult';
 const reactTemplate = 'test/test-templates/react-template';
 const nunjucksTemplate = 'test/test-templates/nunjucks-template';
+const logMessage = require('../lib/logMessages.js');
 const log = require('loglevel');
 
 describe('Integration testing generateFromFile() to make sure the result of the generation is not changend comparing to snapshot', () => {
@@ -95,15 +96,9 @@ describe('Integration testing generateFromFile() to make sure the result of the 
     // Check if the files have been overwritten
     await expect(fileContent).toBe(testContent);
     // Check if the log debug message was printed
-    await expect(logSpyDebug).toHaveBeenCalledWith(`noOverwriteGlobs matched`);
-    await expect(logSpyDebug).toHaveBeenCalledWith(`Skipping overwrite for: ${testFilePath}`);
-    // await console.log('All console.log calls:');
+    await expect(logSpyDebug).toHaveBeenCalledWith(logMessage.skipOverwrite(testFilePath));
 
-    // // Print all console.log calls
-    // await log.mock.calls.forEach((call, index) => {
-    //   console.log(`${index + 1}:`, call);
-    // });
-
+    // Clean up
     logSpyDebug.mockRestore();
   });
 });

41622aca94d54239e7242966f948279ebcfca58b
ayushnau <ayushnautiyaldevelopr@gmail.com>
2024-07-22 12:42:51 +0530

    reverted the unwated change.

--- apps/generator/test/test-templates/react-template/__transpiled/test-file.md.js
+++ apps/generator/test/test-templates/react-template/__transpiled/test-file.md.js
@@ -1,22 +1,29 @@
 'use strict';
 
 require('source-map-support/register');
-var generatorReactSdk = require('@asyncapi/generator-react-sdk');
-var jsxRuntime = require('react/cjs/react-jsx-runtime.production.min');
+const generatorReactSdk = require('@asyncapi/generator-react-sdk');
+const jsxRuntime = require('react/cjs/react-jsx-runtime.production.min');
 
-function testFile_md ({
-  asyncapi,
-  params
-}) {
-  return /*#__PURE__*/jsxRuntime.jsxs(generatorReactSdk.File, {
-    name: "test-file.md",
-    children: [/*#__PURE__*/jsxRuntime.jsx(generatorReactSdk.Text, {
-      children: "This is a markdown file for my application."
-    }), /*#__PURE__*/jsxRuntime.jsxs(generatorReactSdk.Text, {
-      children: ["App name is: **", asyncapi.info().title(), "**"]
-    }), /*#__PURE__*/jsxRuntime.jsxs(generatorReactSdk.Text, {
-      children: ["Version ", params.version, " running on ", params.mode, " mode "]
-    })]
+function testFile_md({ asyncapi, params }) {
+  return /*#__PURE__*/ jsxRuntime.jsxs(generatorReactSdk.File, {
+    name: 'test-file.md',
+    children: [
+      /*#__PURE__*/ jsxRuntime.jsx(generatorReactSdk.Text, {
+        children: 'This is a markdown file for my application.',
+      }),
+      /*#__PURE__*/ jsxRuntime.jsxs(generatorReactSdk.Text, {
+        children: ['App name is: **', asyncapi.info().title(), '**'],
+      }),
+      /*#__PURE__*/ jsxRuntime.jsxs(generatorReactSdk.Text, {
+        children: [
+          'Version ',
+          params.version,
+          ' running on ',
+          params.mode,
+          ' mode ',
+        ],
+      }),
+    ],
   });
 }
 

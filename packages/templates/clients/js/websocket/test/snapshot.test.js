9b0d01f99eca1dacabdb27e594f9723d22ce9f98
Light13008 <sarupatil0001@gmail.com>
2025-03-19 01:47:20 +0530

    README Generation logic was added in Snapshot.test.js

--- packages/templates/clients/js/websocket/test/snapshot.test.js
+++ packages/templates/clients/js/websocket/test/snapshot.test.js
@@ -13,10 +13,11 @@ const template = './';
 
 describe('testing if generated client match snapshot', () => {
   jest.setTimeout(100000);
-  
+
   // FOR POSTMAN CLIENT
-  it('generate simple client for postman echo', async () => {
+  it('generate simple client and README for postman echo', async () => {
     const testOutputFile = 'client-postman.js';
+    const readmeFile = 'README.md';
 
     const generator = new Generator(template, testResultPathPostman, {
       forceWrite: true,
@@ -30,12 +31,15 @@ describe('testing if generated client match snapshot', () => {
 
     const client = await readFile(path.join(testResultPathPostman, testOutputFile), 'utf8');
     expect(client).toMatchSnapshot();
+
+    const readme = await readFile(path.join(testResultPathPostman, readmeFile), 'utf8');
+    expect(readme).toMatchSnapshot();
   });
 
-
   // FOR HOPPSCOTCH CLIENT
-  it('generate simple client for hoppscotch echo', async () => {
+  it('generate simple client and README for hoppscotch echo', async () => {
     const testOutputFile = 'client-hoppscotch.js';
+    const readmeFile = 'README.md';
 
     const generator = new Generator(template, testResultPathHoppscotch, {
       forceWrite: true,
@@ -49,5 +53,8 @@ describe('testing if generated client match snapshot', () => {
 
     const client = await readFile(path.join(testResultPathHoppscotch, testOutputFile), 'utf8');
     expect(client).toMatchSnapshot();
+
+    const readme = await readFile(path.join(testResultPathHoppscotch, readmeFile), 'utf8');
+    expect(readme).toMatchSnapshot();
   });
 });

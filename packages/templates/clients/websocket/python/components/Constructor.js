85ddcd78046a936ffd959f7efc9ea34955d4ee3c
Lukasz Gornicki <lpgornicki@gmail.com>
2025-04-03 15:09:17 +0200

    refactor: enable `url` for clients constructors (#1468)
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>

--- packages/templates/clients/websocket/python/components/Constructor.js
+++ packages/templates/clients/websocket/python/components/Constructor.js
@@ -4,8 +4,15 @@ export function Constructor({ serverUrl }) {
   return (
     <Text indent={2} newLines={2}>
       {
-        `def __init__(self):
-    self.url = "${serverUrl}"
+        `def __init__(self, url: str = "${serverUrl}"):
+    """
+    Constructor to initialize the WebSocket client.
+
+    Args:
+        url (str, optional): The WebSocket server URL. Use it if the server URL is 
+        different from the default one taken from the AsyncAPI document.
+    """
+    self.url = url
     self.ws_app = None  # Instance of WebSocketApp
     self.message_handlers = []      # Callables for incoming messages
     self.error_handlers = []        # Callables for errors

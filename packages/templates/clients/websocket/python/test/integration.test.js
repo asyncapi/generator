d4a0d9a61bb1ffe4a9a00b6c0a97fcaef57deb72
Aditya Vashisht <avdelhistar@gmail.com>
2025-04-06 13:56:47 +0000

    test: move shared fixtures for websocket clients and update integration tests

--- packages/templates/clients/websocket/python/test/integration.test.js
+++ packages/templates/clients/websocket/python/test/integration.test.js
@@ -5,53 +5,50 @@
 const path = require('path');
 const { readFile } = require('fs').promises;
 const Generator = require('@asyncapi/generator');
-const asyncapi_v3_path_postman = path.resolve(__dirname, './__fixtures__/asyncapi-postman-echo.yml');
-const asyncapi_v3_path_hoppscotch = path.resolve(__dirname, './__fixtures__/asyncapi-hoppscotch-echo.yml');
+
+const asyncapi_v3_path_postman = path.resolve(__dirname, '../../../test/__fixtures__/asyncapi-postman-echo.yml');
+const asyncapi_v3_path_hoppscotch = path.resolve(__dirname, '../../../test/__fixtures__/asyncapi-hoppscotch-echo.yml');
 const testResultPath = path.resolve(__dirname, './temp/snapshotTestResult');
 const template = path.resolve(__dirname, '../');
 
-describe('testing if generated client match snapshot', () => {
+describe('Python WebSocket client generation', () => {
   jest.setTimeout(100000);
-  
-  it('generate simple client for postman echo', async () => {
+
+  it('generates client for Postman Echo WebSocket', async () => {
     const testOutputFiles = ['client_postman.py', 'requirements.txt'];
 
     const generator = new Generator(template, testResultPath, {
       forceWrite: true,
       templateParams: {
         server: 'echoServer',
-        clientFileName: testOutputFiles[0]
-      }
+        clientFileName: testOutputFiles[0],
+      },
     });
 
     await generator.generateFromFile(asyncapi_v3_path_postman);
 
-    for (const testOutputFile of testOutputFiles) {
-      const filePath = path.join(testResultPath, testOutputFile);
-      const content = await readFile(filePath, 'utf8');
-
-      expect(content).toMatchSnapshot(testOutputFile);
+    for (const file of testOutputFiles) {
+      const content = await readFile(path.join(testResultPath, file), 'utf8');
+      expect(content).toMatchSnapshot(file);
     }
   });
 
-  it('generate simple client for hoppscotch echo', async () => {
+  it('generates client for Hoppscotch Echo WebSocket', async () => {
     const testOutputFiles = ['client_hoppscotch.py', 'requirements.txt'];
 
     const generator = new Generator(template, testResultPath, {
       forceWrite: true,
       templateParams: {
         server: 'echoServer',
-        clientFileName: testOutputFiles[0]
-      }
+        clientFileName: testOutputFiles[0],
+      },
     });
 
     await generator.generateFromFile(asyncapi_v3_path_hoppscotch);
 
-    for (const testOutputFile of testOutputFiles) {
-      const filePath = path.join(testResultPath, testOutputFile);
-      const content = await readFile(filePath, 'utf8');
-
-      expect(content).toMatchSnapshot(testOutputFile);
+    for (const file of testOutputFiles) {
+      const content = await readFile(path.join(testResultPath, file), 'utf8');
+      expect(content).toMatchSnapshot(file);
     }
   });
 });

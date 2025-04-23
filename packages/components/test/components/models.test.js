479124063c9d91c8eda492c63d0253c68bbf6c31
thulieblack <sibanda.thulie@gmail.com>
2025-04-23 10:05:31 +0200

    update the models test script

--- packages/components/test/components/models.test.js
+++ packages/components/test/components/models.test.js
@@ -1,11 +1,12 @@
 import path from 'path';
+import { render } from '@asyncapi/generator-react-sdk';
 import { Models } from '../../src/index';
 import { Parser, fromFile } from '@asyncapi/parser';
 
 const parser = new Parser();
 const asyncapi_v3_path = path.resolve(__dirname, '../__fixtures__/asyncapi-v3.yml');
 
-describe('Integration Tests for models function', () => {
+describe('Models component rendering as string', () => {
   let parsedAsyncAPIDocument;
 
   beforeAll(async () => {
@@ -13,15 +14,18 @@ describe('Integration Tests for models function', () => {
     parsedAsyncAPIDocument = parseResult.document;
   });
 
-  test('renders default as Python models', async () => {
-    const result = await Models({asyncapi: parsedAsyncAPIDocument});
-
-    expect(result).toMatchSnapshot();
+  test('renders default Python models as plain string output', async () => {
+    const component = await Models({ asyncapi: parsedAsyncAPIDocument });
+    const result = render(component);
+    const actual = result.trim();
+    expect(actual).toMatchSnapshot();
   });
 
-  test('renders Csharp models', async () => {
-    const result = await Models({asyncapi: parsedAsyncAPIDocument, language: 'csharp'});
-
-    expect(result).toMatchSnapshot();
+  test('renders C# models as plain string output', async () => {
+    const component = await Models({ asyncapi: parsedAsyncAPIDocument, language: 'csharp' });
+    const result = render(component);
+    const actual = result.trim();
+    expect(actual).toMatchSnapshot();
   });
 });
+44

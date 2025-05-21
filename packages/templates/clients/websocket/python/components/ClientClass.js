8afd069872df26a5b285c49e9ddefd796d043fd1
Aditya Vashisht <avdelhistar@gmail.com>
2025-05-21 13:12:36 +0000

    Describe your changes here

--- packages/templates/clients/websocket/python/components/ClientClass.js
+++ packages/templates/clients/websocket/python/components/ClientClass.js
@@ -1,4 +1,5 @@
 import { Text } from '@asyncapi/generator-react-sdk';
+import { getClientName, getServerUrl, getServer } from '@asyncapi/generator-helpers';
 import { Constructor } from './Constructor';
 import { Connect } from './Connect';
 import { RegisterMessageHandler } from './RegisterMessageHandler';
@@ -10,6 +11,12 @@ import { RegisterOutgoingProcessor } from './RegisterOutgoingProcessor';
 import { HandleError } from './HandleError';
 
 export function ClientClass({ clientName, serverUrl, title }) {
+  const info = asyncapi.info();
+  const clientName = getClientName(info);
+  const server = getServer(asyncapi.servers(), params.server);
+  const serverUrl = getServerUrl(server);
+  const title = info.title();
+
   return (
     <Text>
       <Text newLines={2}>
--- packages/templates/clients/websocket/python/template/client.py.js
+++ packages/templates/clients/websocket/python/template/client.py.js
@@ -1,5 +1,5 @@
 import { File } from '@asyncapi/generator-react-sdk';
-import { getClientName, getServerUrl, getServer } from '@asyncapi/generator-helpers';
+import { getServer } from '@asyncapi/generator-helpers';
 import { FileHeaderInfo } from '../components/FileHeaderInfo';
 import { Requires } from '../components/Requires';
 import { ClientClass } from '../components/ClientClass';
@@ -7,18 +7,17 @@ import { ClientClass } from '../components/ClientClass';
 export default function ({ asyncapi, params }) {
   const server = getServer(asyncapi.servers(), params.server);
   const info = asyncapi.info();
-  const title = info.title();
-  const clientName = getClientName(info);
-  const serverUrl = getServerUrl(server);
+
+
+
   return (
-    // The clientFileName default values can be found and modified under the package.json
     <File name={params.clientFileName}>
       <FileHeaderInfo
         info={info}
         server={server}
       />
       <Requires />
-      <ClientClass clientName={clientName} serverUrl={serverUrl} title={title} />
+      <ClientClass asyncapi={asyncapi} params={params} />
     </File>
   );
 }

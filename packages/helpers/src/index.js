bfae922d652d15df9c9e23d7ad90195978014baa
Mohan Kumar <121867882+ItshMoh@users.noreply.github.com>
2025-06-03 11:25:24 +0530

    Update packages/helpers/src/index.js
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>

--- packages/helpers/src/index.js
+++ packages/helpers/src/index.js
@@ -1,6 +1,6 @@
 const { getMessageExamples, getOperationMessages } = require('./operations');
 const { getServerUrl, getServer }  = require('./servers');
-const { getClientName, listFiles, getInfo, toSnakeCase ,getTitle} = require('./utils');
+const { getClientName, listFiles, getInfo, toSnakeCase, getTitle} = require('./utils');
 const { getQueryParams } = require('./bindings');
 
 module.exports = {

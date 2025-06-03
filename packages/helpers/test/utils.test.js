056f0ed77dd08a15bf16e6403323b273109417a6
Mohan Kumar <121867882+ItshMoh@users.noreply.github.com>
2025-06-03 19:11:25 +0530

    chore: add the `getTitle` helper (#1568)
    Co-authored-by: ItshMoh <mohan.kumar.min22@itbhu.ac.in>
    Co-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>

--- packages/helpers/test/utils.test.js
+++ packages/helpers/test/utils.test.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { Parser, fromFile } = require('@asyncapi/parser');
-const { getClientName, getInfo, toSnakeCase } = require('@asyncapi/generator-helpers');
+const { getClientName, getInfo, getTitle, toSnakeCase } = require('@asyncapi/generator-helpers');
 
 const parser = new Parser();
 const asyncapi_v3_path = path.resolve(__dirname, './__fixtures__/asyncapi-websocket-query.yml');
@@ -14,33 +14,30 @@ describe('getClientName integration test with AsyncAPI', () => {
   });
 
   it('should generate correct client name for the provided AsyncAPI info object without appendClientSuffix', () => {
-    const info = parsedAsyncAPIDocument.info();
     const appendClientSuffix = false;
     const customClientName = '';
 
-    const clientName = getClientName(info, appendClientSuffix, customClientName);
+    const clientName = getClientName(parsedAsyncAPIDocument, appendClientSuffix, customClientName);
 
     // Example assertion: Check if the name is formatted correctly
     expect(clientName).toBe('GeminiMarketDataWebsocketAPI');
   });
 
   it('should generate correct client name for the provided AsyncAPI info object with appendClientSuffix', () => {
-    const info = parsedAsyncAPIDocument.info();
     const appendClientSuffix = true;
     const customClientName = '';
 
-    const clientName = getClientName(info, appendClientSuffix, customClientName);
+    const clientName = getClientName(parsedAsyncAPIDocument, appendClientSuffix, customClientName);
 
     // Example assertion: Check if the name is formatted correctly
     expect(clientName).toBe('GeminiMarketDataWebsocketAPIClient');
   });
 
   it('should return customClientName', () => {
-    const info = parsedAsyncAPIDocument.info();
     const appendClientSuffix = false;
     const customClientName = 'GeminiClient';
 
-    const clientName = getClientName(info, appendClientSuffix, customClientName);
+    const clientName = getClientName(parsedAsyncAPIDocument, appendClientSuffix, customClientName);
 
     // Example assertion: Check if the name is formatted correctly
     expect(clientName).toBe(customClientName);
@@ -79,6 +76,45 @@ describe('getInfo integration test with AsyncAPI', () => {
     expect(() => {
       getInfo(null);
     }).toThrow('Make sure you pass AsyncAPI document as an argument.');
+  });
+});
+
+describe('getTitle integration test with AsyncAPI', () => {
+  let parsedAsyncAPIDocument;
+
+  beforeAll(async () => {
+    const parseResult = await fromFile(parser, asyncapi_v3_path).parse();
+    parsedAsyncAPIDocument = parseResult.document;
+  });
+
+  it('should return the exact title parameter when exists', () => {
+    const info = parsedAsyncAPIDocument.info();
+    const expectedTitle = info.title();
+    const actualTitle = getTitle(parsedAsyncAPIDocument);
+    expect(actualTitle).toStrictEqual(expectedTitle);
+  });
+
+  it('should throw error when title function does not exist', () => {
+    const asyncAPIDocWithoutTitle = {
+      info: () => ({
+        // info object without title method
+      })
+    };
+    expect(() => {
+      getTitle(asyncAPIDocWithoutTitle);
+    }).toThrow('Provided AsyncAPI document info field doesn\'t contain title.');
+  });
+
+  it('should throw error when title is an empty string', () => {
+    const asyncAPIDocWithEmptyTitle = {
+      info: () => ({
+        title: () => ''
+      })
+    };
+
+    expect(() => {
+      getTitle(asyncAPIDocWithEmptyTitle);
+    }).toThrow('AsyncAPI document title cannot be an empty string.');
   });
 });
 

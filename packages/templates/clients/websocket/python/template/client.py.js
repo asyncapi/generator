e00cba7d7e7f05bf53a9c6f3cef7626cc44a8332
Aditya Vashisht <avdelhistar@gmail.com>
2025-06-04 18:51:27 +0530

    Update client.py.js

--- packages/templates/clients/websocket/python/template/client.py.js
+++ packages/templates/clients/websocket/python/template/client.py.js
@@ -1,15 +1,15 @@
 import { File } from '@asyncapi/generator-react-sdk';
-import { getClientName, getServerUrl, getServer, getQueryParams, getInfo, getTitle } from '@asyncapi/generator-helpers';
+import { getClientName, getServerUrl, getServer, getQueryParams } from '@asyncapi/generator-helpers';
 import { FileHeaderInfo } from '../components/FileHeaderInfo';
 import { Requires } from '../components/Requires';
 import { ClientClass } from '../components/ClientClass';
 
 export default function ({ asyncapi, params }) {
   const server = getServer(asyncapi.servers(), params.server);
-  const info = getInfo(asyncapi);
-  const title = getTitle(asyncapi);
+  const info = asyncapi.info();
+  const title = info.title;
   const queryParams = getQueryParams(asyncapi.channels());
-  const clientName = getClientName(asyncapi, params.appendClientSuffix, params.customClientName);
+  const clientName = getClientName(info, params.appendClientSuffix, params.customClientName);
   const serverUrl = getServerUrl(server);
 
   return (
@@ -19,7 +19,8 @@ export default function ({ asyncapi, params }) {
         server={server}
       />
       <Requires query={queryParams} />
-      <ClientClass clientName={clientName} serverUrl={serverUrl} title={title} queryParams={queryParams} operations={operations} />
+          <Requires query={queryParams} />
+      <ClientClass clientName={clientName} serverUrl={serverUrl} title={title} queryParams={queryParams} />
     </File>
   );
 }

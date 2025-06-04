4de91fc039337d5d850bd0ba179c79a0bbd1cf68
Aditya Vashisht <avdelhistar@gmail.com>
2025-06-04 18:54:22 +0530

    Update ClientClass.js

--- packages/templates/clients/websocket/python/components/ClientClass.js
+++ packages/templates/clients/websocket/python/components/ClientClass.js
@@ -10,8 +10,7 @@ import { CloseConnection } from './CloseConnection';
 import { RegisterOutgoingProcessor } from './RegisterOutgoingProcessor';
 import { HandleError } from './HandleError';
 
-export function ClientClass({ clientName, serverUrl, title, queryParams, operations }) {
-  const sendOperations = operations.filterBySend();  //just to pass the commiting chnage from codespace //hi //hellooo //hi
+export function ClientClass({ clientName, serverUrl, title, queryParams, }) {
   return (
     <Text>
       <Text newLines={2}>
@@ -24,8 +23,7 @@ export function ClientClass({ clientName, serverUrl, title, queryParams, operati
       <RegisterOutgoingProcessor />
       <HandleMessage />
       <HandleError />
-      <SendOperation sendOperations={sendOperations} clientName={clientName} />
-      <Send sendOperations={sendOperations} />
+      <SendEchoMessage />
       <CloseConnection />
     </Text>
   );

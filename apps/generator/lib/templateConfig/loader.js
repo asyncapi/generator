fe4253761ab805de09c7edaeeb7d74da6ec1ea9a
ItsRoy69 <jyotirmoyroy649@gmail.com>
2025-06-09 17:54:11 +0530

    removed entirely the loader and templateconfig from generator.js
    Signed-off-by: ItsRoy69 <jyotirmoyroy649@gmail.com>

--- apps/generator/lib/templateConfig/loader.js
+++ apps/generator/lib/templateConfig/loader.js
@@ -1,79 +1,62 @@
 const path = require('path');
 const { readFile } = require('../utils');
 const log = require('loglevel');
-const fs = require('fs');
 
 const CONFIG_FILENAME = 'package.json';
 
 /**
- * Loads template configuration from either .ageneratorrc or package.json
- * @param {string} templateDir - Path to the template directory
- * @returns {Promise<Object>} Template configuration object
- * @throws {Error} If templateDir is invalid or directory doesn't exist
- */
-async function loadTemplateConfig(templateDir) {
-  if (!templateDir || typeof templateDir !== 'string') {
-    throw new Error('templateDir must be a valid string path');
-  }
-
-  if (!fs.existsSync(templateDir)) {
-    throw new Error(`Template directory does not exist: ${templateDir}`);
-  }
-
-  if (!fs.statSync(templateDir).isDirectory()) {
-    throw new Error(`Path is not a directory: ${templateDir}`);
-  }
-
-  let templateConfig = {};
+  * Loads the template configuration.
+  * @private
+  */
+async function loadTemplateConfig() {
+  this.templateConfig = {};
 
   // Try to load config from .ageneratorrc
   try {
-    const rcConfigPath = path.resolve(templateDir, '.ageneratorrc');
+    const rcConfigPath = path.resolve(this.templateDir, '.ageneratorrc');
     const yaml = await readFile(rcConfigPath, { encoding: 'utf8' });
     const yamlConfig = require('js-yaml').load(yaml);
-    templateConfig = yamlConfig || {};
+    this.templateConfig = yamlConfig || {};
 
-    log.debug('Template configuration loaded from .ageneratorrc');
-    return templateConfig;
+    await this.loadDefaultValues();
+    return;
   } catch (rcError) {
+    // console.error('Could not load .ageneratorrc file:', rcError);
     log.debug('Could not load .ageneratorrc file:', rcError);
     // Continue to try package.json if .ageneratorrc fails
   }
 
   // Try to load config from package.json
   try {
-    const configPath = path.resolve(templateDir, CONFIG_FILENAME);
+    const configPath = path.resolve(this.templateDir, CONFIG_FILENAME);
     const json = await readFile(configPath, { encoding: 'utf8' });
     const generatorProp = JSON.parse(json).generator;
-    templateConfig = generatorProp || {};
-
-    log.debug('Template configuration loaded from package.json');
+    this.templateConfig = generatorProp || {};
   } catch (packageError) {
+    // console.error('Could not load generator config from package.json:', packageError);
     log.debug('Could not load generator config from package.json:', packageError);
   }
 
-  return templateConfig;
+  await this.loadDefaultValues();
 }
 
 /**
- * Loads default values of parameters from template config
- * @param {Object} templateConfig - The template configuration object
- * @param {Object} templateParams - The template parameters object to modify
+ * Loads default values of parameters from template config. If value was already set as parameter it will not be
+ * overriden.
+ * @private
  */
-function loadDefaultValues(templateConfig, templateParams) {
-  const parameters = templateConfig.parameters;
+async function loadDefaultValues() {
+  const parameters = this.templateConfig.parameters;
   const defaultValues = Object.keys(parameters || {}).filter(key => parameters[key].default);
 
-  defaultValues
-    .filter(dv => templateParams[dv] === undefined)
-    .forEach(dv => {
-      Object.defineProperty(templateParams, dv, {
-        enumerable: true,
-        get() {
-          return parameters[dv].default;
-        }
-      });
-    });
+  defaultValues.filter(dv => this.templateParams[dv] === undefined).forEach(dv =>
+    Object.defineProperty(this.templateParams, dv, {
+      enumerable: true,
+      get() {
+        return parameters[dv].default;
+      }
+    })
+  );
 }
 
 module.exports = {

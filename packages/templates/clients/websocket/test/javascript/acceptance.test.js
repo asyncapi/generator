b2210b514cf75d4ce004d23678183173e47451aa
Adi-204 <adiboghawala@gmail.com>
2025-06-17 00:48:18 +0530

    chore: fix CI/CD

--- packages/templates/clients/websocket/test/javascript/acceptance.test.js
+++ packages/templates/clients/websocket/test/javascript/acceptance.test.js
@@ -75,12 +75,13 @@ describe('client - sender tests', () => {
     //await delay(100000);
     const expectedMessage = 'Sending acceptance test message';
 
-    wsServer.on('connection', (socket) => {
-      /*
-      * TEST: sending message with generated function to microcks test client that connected to our custom server
-      */
-      WSClient.sendEchoMessage(expectedMessage, socket);
-    });
+   wsServer.on('connection', async (socket) => {
+      try {
+        await WSClient.sendEchoMessage(expectedMessage, socket);
+      } catch (err) {
+        console.error('Error in sending message:', err);
+      }
+   });
 
     // Start test in Microcks
     const response = await fetch(microcksTestEndpoint, {

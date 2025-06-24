9292ecd9936bbdce98c9522c404aa5e29141d1e0
Lukasz Gornicki <lpgornicki@gmail.com>
2025-06-24 17:45:29 +0200

    docs: update slack example with real token

--- packages/templates/clients/websocket/python/example-slack.py
+++ packages/templates/clients/websocket/python/example-slack.py
@@ -4,13 +4,13 @@ import time
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'test'))
 
-from temp.snapshotTestResult.client_slack.client import SlackWebsocketAPIClientClient
+from temp.snapshotTestResult.client_slack.client import SlackWebsocketAPIClient
 
 def custom_message_handler(message):
     print(f"\033[94mIncomming event from Slack\033[0m: {message}")
 
 def main():
-    client = SlackWebsocketAPIClientClient()
+    client = SlackWebsocketAPIClient()
     client.register_message_handler(custom_message_handler)
 
     client.connect()

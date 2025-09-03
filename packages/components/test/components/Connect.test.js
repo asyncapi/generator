b8d9ba8ea0c3f1cc67ee61211b274d3e26b9b97f
Adi-204 <adiboghawala@gmail.com>
2025-09-03 15:33:33 +0530

    refactor connect test

--- packages/components/test/components/Connect.test.js
+++ packages/components/test/components/Connect.test.js
@@ -2,32 +2,11 @@ import { render } from '@asyncapi/generator-react-sdk';
 import { Connect } from '../../src/index';
 
 describe('Testing of Connect function', () => {
-  test('render javascript Connect method', () => {
+  const languages = ['javascript', 'python', 'dart'];
+  it.each(languages)('render %s Connect method', (language) => {
     const result = render(
       <Connect 
-        language='javascript' 
-        title='HoppscotchEchoWebSocketClient'
-      />
-    );
-    const actual = result.trim();
-    expect(actual).toMatchSnapshot();
-  });
-
-  test('render python Connect method', () => {
-    const result = render(
-      <Connect 
-        language='python'
-        title='HoppscotchEchoWebSocketClient'
-      />
-    );
-    const actual = result.trim();
-    expect(actual).toMatchSnapshot();
-  });
-
-  test('render dart Connect method', () => {
-    const result = render(
-      <Connect 
-        language='dart'
+        language={language} 
         title='HoppscotchEchoWebSocketClient'
       />
     );

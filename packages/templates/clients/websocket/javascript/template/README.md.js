414ea8522abe3a495291313d5feb77fd089e72d3
thulieblack <sibanda.thulie@gmail.com>
2025-09-26 11:21:19 +0200

    update keys props

--- packages/templates/clients/websocket/javascript/template/README.md.js
+++ packages/templates/clients/websocket/javascript/template/README.md.js
@@ -1,25 +1,30 @@
 import { File, Text } from '@asyncapi/generator-react-sdk';
 import { BaseInfo } from '../../../../../components/Info';
 import { Overview } from '../../../../../components/Overview';
-import { CoreMethods} from '../../../../../components/CoreMethods';
+import { CoreMethods } from '../../../../../components/CoreMethods';
 import { Usage } from '../../../../../components/Usage';
 import { AvailableOperations } from '../components/AvailableOperations';
 
 export default function({ asyncapi, params }) {
   const { info, clientName, title, serverUrl } = BaseInfo(asyncapi, params);
 
-
   const operations = asyncapi.operations().all();
 
-    return (
+  return (
     <File name="README.md">
-      <Text newLines={2}>
-        {`# ${title}\n`}
-      </Text>
-      <Overview info={info} title={title} serverUrl={serverUrl} />
-      <Usage clientName={clientName} clientFileName={params.clientFileName} language="javascript" />
-      <CoreMethods language="javascript" />
-      <AvailableOperations operations={operations} />
+      <>
+        <Text newLines={2}>
+          {`# ${title}\n`}
+        </Text>
+        <Overview info={info} title={title} serverUrl={serverUrl} />
+        <Usage
+          clientName={clientName}
+          clientFileName={params.clientFileName}
+          language="javascript"
+        />
+        <CoreMethods language="javascript" />
+        <AvailableOperations operations={operations} />
+      </>
     </File>
   );
 }

8c32372ac17dd684e4487504992dffd7afedf6af
thulieblack <sibanda.thulie@gmail.com>
2025-10-16 21:00:46 +0200

    fix import structure

--- packages/templates/clients/websocket/javascript/template/README.md.js
+++ packages/templates/clients/websocket/javascript/template/README.md.js
@@ -2,15 +2,8 @@ import { GenerateReadMe } from '../../../../../components/src/components/readme/
 import { AvailableOperations } from '../components/AvailableOperations';
 
 export default function({ asyncapi, params }) {
-  const readme = <GenerateReadMe asyncapi={asyncapi} params={params} language="javascript" />;
-
-  const operations = asyncapi.operations().all();
-
-  return (
-    <>
-      {readme}
-      <AvailableOperations operations={operations} />
-    </>
-  );
+  
+  return GenerateReadMe({asyncapi, params, language:'javascript', AvailableOperations});
 }
+
 
--- packages/templates/clients/websocket/python/template/README.md.js
+++ packages/templates/clients/websocket/python/template/README.md.js
@@ -1,5 +1,6 @@
 import { GenerateReadMe } from '../../../../../components/src/components/readme/Readme';
 
 export default function({ asyncapi, params }) {
-  return <GenerateReadMe asyncapi={asyncapi} params={params} language="python" />;
+ 
+  return GenerateReadMe({asyncapi, params, language:'python'});
 }

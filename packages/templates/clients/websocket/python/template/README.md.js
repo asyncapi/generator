b7d5ef761ec4c22ac492a5e37906b694857798e9
thulieblack <sibanda.thulie@gmail.com>
2025-10-16 21:18:37 +0200

    fix linting

--- packages/templates/clients/websocket/python/template/README.md.js
+++ packages/templates/clients/websocket/python/template/README.md.js
@@ -1,5 +1,5 @@
 import { GenerateReadMe } from '../../../../../components/src/components/readme/Readme';
 
 export default function({ asyncapi, params }) {
-  return GenerateReadMe({asyncapi, params, language:'python' });
+  return GenerateReadMe({asyncapi, params, language: 'python' });
 }

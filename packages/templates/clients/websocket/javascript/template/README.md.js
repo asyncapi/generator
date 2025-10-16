f06630f727fa39f6943589e52da61ee27fdb530e
thulieblack <sibanda.thulie@gmail.com>
2025-10-16 17:04:02 +0200

    update-readme

--- packages/templates/clients/websocket/javascript/template/README.md.js
+++ packages/templates/clients/websocket/javascript/template/README.md.js
@@ -1,5 +1,16 @@
 import { GenerateReadMe } from '../../../../../components/src/components/readme/Readme';
+import { AvailableOperations } from '../components/AvailableOperations';
 
 export default function({ asyncapi, params }) {
-  return GenerateReadMe({ asyncapi, params, language: 'javascript' });
+  const readme = <GenerateReadMe asyncapi={asyncapi} params={params} language="javascript" />;
+
+  const operations = asyncapi.operations().all();
+
+  return (
+    <>
+      {readme}
+      <AvailableOperations operations={operations} />
+    </>
+  );
 }
+

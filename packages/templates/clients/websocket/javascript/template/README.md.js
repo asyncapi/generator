aab695f9115da90616b24395a283170defaef8d8
thulieblack <sibanda.thulie@gmail.com>
2025-10-16 15:47:07 +0200

    movefiles

--- packages/templates/clients/websocket/javascript/template/README.md.js
+++ packages/templates/clients/websocket/javascript/template/README.md.js
@@ -1,30 +1,5 @@
-import { File, Text } from '@asyncapi/generator-react-sdk';
-import { BaseInfo } from '../../../../../components/Info';
-import { Overview } from '../../../../../components/Overview';
-import { CoreMethods } from '../../../../../components/CoreMethods';
-import { Usage } from '../../../../../components/Usage';
-import { AvailableOperations } from '../components/AvailableOperations';
+import { GenerateReadMe } from '../../../../../components/src/components/readme/Readme';
 
 export default function({ asyncapi, params }) {
-  const { info, clientName, title, serverUrl } = BaseInfo(asyncapi, params);
-
-  const operations = asyncapi.operations().all();
-
-  return (
-    <File name="README.md">
-      <>
-        <Text newLines={2}>
-          {`# ${title}`}
-        </Text>
-        <Overview info={info} title={title} serverUrl={serverUrl} />
-        <Usage
-          clientName={clientName}
-          clientFileName={params.clientFileName}
-          language="javascript"
-        />
-        <CoreMethods language="javascript" />
-        <AvailableOperations operations={operations} />
-      </>
-    </File>
-  );
+  return GenerateReadMe({ asyncapi, params, language: 'javascript' });
 }
--- packages/templates/clients/websocket/python/template/README.md.js
+++ packages/templates/clients/websocket/python/template/README.md.js
@@ -1,20 +1,5 @@
-import { File, Text } from '@asyncapi/generator-react-sdk';
-import { BaseInfo } from '../../../../../components/Info';
-import { Overview } from '../../../../../components/Overview';
-import { Installation } from '../../../../../components/Installation';
-import { Usage } from '../../../../../components/Usage';
-import { CoreMethods} from '../../../../../components/CoreMethods';
+import { GenerateReadMe } from '../../../../../components/src/components/readme/Readme';
 
 export default function({ asyncapi, params }) {
-  const { info, clientName, title, serverUrl } = BaseInfo(asyncapi, params);
-
-  return (
-    <File name="README.md">
-      <Text newLines={2}>{`# ${title}`}</Text>
-      <Overview info={info} title={title} serverUrl={serverUrl} />
-      <Installation />
-      <Usage clientName={clientName} clientFileName={params.clientFileName} language="python" />
-      <CoreMethods language="python" />
-    </File>
-  );
+  return GenerateReadMe({ asyncapi, params, language: 'python' });
 }

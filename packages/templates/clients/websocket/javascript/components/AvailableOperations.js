0ac8dfaa69f4fdced5624b6bd67613a3d4afb4a4
thulieblack <sibanda.thulie@gmail.com>
2025-11-12 15:22:45 +0200

    update-comp

--- packages/templates/clients/websocket/javascript/components/AvailableOperations.js
+++ packages/templates/clients/websocket/javascript/components/AvailableOperations.js
@@ -9,8 +9,8 @@ export function AvailableOperations({ operations }) {
   return (
     <>
       <Text newLines={2}>### Available Operations</Text>
-      {operations.map((operation) => (
-        <Text newLines={2}>
+      {operations.map((operation, index) => (
+        <Text key={index} newLines={2}>
           <OperationHeader operation={operation} />
           <MessageExamples operation={operation} />
         </Text>

d1d83db5b928347a6caf69dcdc61e106c1f59cd0
Varshitha Besthavemula <varshithabesthavemula@gmail.com>
2026-01-17 22:19:01 +0530

    add jsdoc for escapeType(jsdoc-helper)

--- packages/components/jsdoc2md-helpers/jsdoc-helper.js
+++ packages/components/jsdoc2md-helpers/jsdoc-helper.js
@@ -1,4 +1,9 @@
 module.exports = {
+/**
+  * Escapes pipe characters in a JSDoc type string for Markdown output.
+  * `@param` {string | null | undefined} type
+  * `@returns` {string | null | undefined}
+  */
   escapeType: function(type) {
     return type ? type.replace(/\|/g, '&#124;') : type;
   }

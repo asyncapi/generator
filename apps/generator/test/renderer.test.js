08c0587d27e1fd376f31a9d89c88c0f668ef6bf3
muralimadhava96-ui <muralimadhava96@gmail.com>
2026-03-01 21:42:24 +0530

    Update renderer.test.js

--- apps/generator/test/renderer.test.js
+++ apps/generator/test/renderer.test.js
@@ -1,40 +1,119 @@
 /* eslint-disable sonarjs/no-duplicate-string */
-const { 
+
+jest.mock('../lib/utils');
+jest.mock('@asyncapi/generator-react-sdk');
+
+const {
   configureReact,
   renderReact,
   saveRenderedReactContent,
 } = require('../lib/renderer/react');
 
-jest.mock('../lib/utils');
-jest.mock('@asyncapi/generator-react-sdk');
-
 describe('React renderer', () => {
-  describe('saveRenderedReactContent', () => {
-    let util;
-    let AsyncReactSDK;
-    beforeAll(() => {
-      util = require('../lib/utils');
-      AsyncReactSDK = require('@asyncapi/generator-react-sdk');
-    });
+  let util;
+  let AsyncReactSDK;
 
-    it('works transpilation', async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    util = require('../lib/utils');
+    AsyncReactSDK = require('@asyncapi/generator-react-sdk');
+  });
+
+  /* ------------------------------------------------------------------ */
+  /* configureReact                                                     */
+  /* ------------------------------------------------------------------ */
+
+  describe('configureReact', () => {
+    it('should transpile React template files', async () => {
       await configureReact('../file', '../dir', '../file');
-      expect(AsyncReactSDK.transpileFiles).toHaveBeenCalledTimes(1);
-    });
 
-    it('works rendering', async () => {
-      await renderReact({}, '../file', {}, '../file', '../dir', '../location', {}, false, '');
+      expect(AsyncReactSDK.transpileFiles).toHaveBeenCalledTimes(1);
+      expect(AsyncReactSDK.transpileFiles).toHaveBeenCalledWith(
+        '../file',
+        '../dir',
+        '../file'
+      );
+    });
+  });
+
+  /* ------------------------------------------------------------------ */
+  /* renderReact                                                        */
+  /* ------------------------------------------------------------------ */
+
+  describe('renderReact', () => {
+    it('should render template using React SDK', async () => {
+      await renderReact(
+        {}, 
+        '../file', 
+        {}, 
+        '../file', 
+        '../dir', 
+        '../location', 
+        {}, 
+        false, 
+        ''
+      );
+
       expect(AsyncReactSDK.renderTemplate).toHaveBeenCalledTimes(1);
     });
-    
-    it('works saving single rendered content', async () => {
+  });
+
+  /* ------------------------------------------------------------------ */
+  /* saveRenderedReactContent                                           */
+  /* ------------------------------------------------------------------ */
+
+  describe('saveRenderedReactContent', () => {
+    it('should save single rendered file', async () => {
       const content = {
         content: 'Content',
         metadata: {
           fileName: 'file.html',
-        }
+        },
       };
 
+      await saveRenderedReactContent(content, '../some/path');
+
+      expect(util.writeFile).toHaveBeenCalledTimes(1);
+      expect(util.writeFile).toHaveBeenCalledWith(
+        '../some/path/file.html',
+        'Content'
+      );
+    });
+
+    it('should save multiple rendered files', async () => {
+      const content = [
+        {
+          content: 'Content1',
+          metadata: { fileName: 'file1.html' },
+        },
+        {
+          content: 'Content2',
+          metadata: { fileName: 'file2.html' },
+        },
+      ];
+
+      await saveRenderedReactContent(content, '../some/path');
+
+      expect(util.writeFile).toHaveBeenCalledTimes(2);
+      expect(util.writeFile).toHaveBeenNthCalledWith(
+        1,
+        '../some/path/file1.html',
+        'Content1'
+      );
+      expect(util.writeFile).toHaveBeenNthCalledWith(
+        2,
+        '../some/path/file2.html',
+        'Content2'
+      );
+    });
+
+    it('should not fail when empty array is provided', async () => {
+      await saveRenderedReactContent([], '../some/path');
+
+      expect(util.writeFile).not.toHaveBeenCalled();
+    });
+  });
+});
       await saveRenderedReactContent(content, '../some/path');
       expect(util.writeFile).toHaveBeenCalledTimes(1);
     });

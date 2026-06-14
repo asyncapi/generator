b713385769b1e71b2231ff13e0ea641323a5e3fc
Musham Sushanth <143796666+SushanthMusham@users.noreply.github.com>
2026-06-14 07:04:22 +0530

    Update packages/helpers/test/bindings.test.js
    importing both getQueryParams,getQueryParamsForAllChannels in same line
    
    Co-authored-by: Adi Boghawala <adiboghawala@gmail.com>

--- packages/helpers/test/bindings.test.js
+++ packages/helpers/test/bindings.test.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { Parser, fromFile } = require('@asyncapi/parser');
-const { getQueryParams,getQueryParamsForAllChannels } = require('@asyncapi/generator-helpers');
+const { getQueryParams, getQueryParamsForAllChannels } = require('@asyncapi/generator-helpers');
 const parser = new Parser();
 const asyncapi_v3_path = path.resolve(__dirname, './__fixtures__/asyncapi-websocket-query.yml');
 

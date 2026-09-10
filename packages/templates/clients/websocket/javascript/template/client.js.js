dbb67f1333accad225dfab1fcead6fc0044766b4
Louis Deconinck <louis.dck@gmail.com>
2026-09-10 19:44:08 +0200

    fix: preserve JSON AsyncAPI path in websocket client
    Signed-off-by: Louis Deconinck <louis.dck@gmail.com>

--- packages/templates/clients/websocket/javascript/template/client.js.js
+++ packages/templates/clients/websocket/javascript/template/client.js.js
@@ -3,14 +3,22 @@ import { getClientName, getServerUrl, getServer, getInfo, getTitle } from '@asyn
 import { FileHeaderInfo, DependencyProvider } from '@asyncapi/generator-components';
 import { ClientClass } from '../components/ClientClass';
 
-export default function ({ asyncapi, params }) {
+export default function ({ asyncapi, params, originalAsyncAPI }) {
   const server = getServer(asyncapi.servers(), params.server);
   const info = getInfo(asyncapi);
   const title = getTitle(asyncapi);
   const clientName = getClientName(asyncapi, params.appendClientSuffix, params.customClientName);
   const serverUrl = getServerUrl(server);
   const sendOperations = asyncapi.operations().filterBySend();
-  const asyncapiFilepath = `${params.asyncapiFileDir}/asyncapi.yaml`;
+  const asyncapiFileExtension = (() => {
+    try {
+      JSON.parse(originalAsyncAPI);
+      return 'json';
+    } catch {
+      return 'yaml';
+    }
+  })();
+  const asyncapiFilepath = `${params.asyncapiFileDir}/asyncapi.${asyncapiFileExtension}`;
   return (
     <File name={params.clientFileName}>
       <FileHeaderInfo
